229ef7ea3ad84286753a898e11c4c9c422695cd4
Steve Manuel <steve@dylib.so>
2022-11-27 08:21:36 -0700

    fix: include fn generic params in wrapped code (#9)

--- examples/count_vowels.rs
+++ examples/count_vowels.rs
@@ -6,14 +6,15 @@ use serde::Serialize;
 const VOWELS: &[char] = &['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'];
 
 #[derive(Serialize)]
-struct TestOutput {
+struct TestOutput<'a> {
     pub count: i32,
     pub config: String,
     pub a: String,
+    pub b: &'a str,
 }
 
 #[plugin_fn]
-pub unsafe fn count_vowels(input: String) -> FnResult<Json<TestOutput>> {
+pub unsafe fn count_vowels<'a>(input: String) -> FnResult<Json<TestOutput<'a>>> {
     let mut count = 0;
     for ch in input.chars() {
         if VOWELS.contains(&ch) {
@@ -26,7 +27,13 @@ pub unsafe fn count_vowels(input: String) -> FnResult<Json<TestOutput>> {
     let a = var::get("a")?.expect("variable 'a' set");
     let a = String::from_utf8(a).expect("string from varible value");
     let config = config::get("thing").expect("'thing' key set in config");
+    let b = "new_value";
 
-    let output = TestOutput { count, config, a };
+    let output = TestOutput {
+        count,
+        config,
+        a,
+        b,
+    };
     Ok(Json(output))
 }

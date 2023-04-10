1f49df30616e0233aaf12d811b60976cee171556
Benjamin Eckel <bhelx@simst.im>
2023-04-09 19:14:24 -0500

    chore: bump version to 0.3.1

--- examples/http.rs
+++ examples/http.rs
@@ -3,8 +3,9 @@
 use extism_pdk::*;
 
 #[plugin_fn]
-pub fn http_get(Json(req): Json<HttpRequest>) -> FnResult<HttpResponse> {
+pub fn http_get(Json(req): Json<HttpRequest>) -> FnResult<String> {
     info!("Request to: {}", req.url);
-    let res = http::request::<()>(&req, None)?;
-    Ok(res)
+    let req1 = &HttpRequest::new(req.url).with_method("POST");
+    let res = http::request::<String>(req1, None)?;
+    Ok(String::from("test"))
 }

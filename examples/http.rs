ead9aef99c66cc566404f4b2cdc2dd09be36f6ea
zach <zach@dylib.so>
2023-04-24 19:03:23 -0700

    fix: http example

--- examples/http.rs
+++ examples/http.rs
@@ -3,9 +3,8 @@
 use extism_pdk::*;
 
 #[plugin_fn]
-pub fn http_get(Json(req): Json<HttpRequest>) -> FnResult<String> {
+pub fn http_get(Json(req): Json<HttpRequest>) -> FnResult<Memory> {
     info!("Request to: {}", req.url);
-    let req1 = &HttpRequest::new(req.url).with_method("POST");
-    let res = http::request::<String>(req1, None)?;
-    Ok(String::from("test"))
+    let res = http::request::<String>(&req, None)?;
+    Ok(res.into_memory().keep())
 }

0f2e8c1f4285635f009cfa42d3173641dab898b1
Benjamin Eckel <bhelx@simst.im>
2023-10-05 12:17:47 -0500

    Some inline docs

--- src/config.rs
+++ src/config.rs
@@ -19,6 +19,20 @@ pub fn get_memory(key: impl AsRef<str>) -> Result<Option<Memory>, Error> {
     })))
 }
 
+/// Gets a config item passed in from the host. This item is read-only
+/// and static throughout the lifetime of the plug-in.
+///
+/// # Arguments
+///
+/// * `key` - A unique string key to identify the variable
+///
+/// # Examples
+///
+/// ```
+/// // let's assume we have a config object: { my_config: 42u32 }
+/// // which is a u32. We can default to 0 first time we fetch it if it's not present
+/// let my_config = config::get("my_config")?.unwrap_or(0u32);
+/// ```
 pub fn get(key: impl AsRef<str>) -> Result<Option<String>, Error> {
     Ok(get_memory(key)?.map(|x| x.to_string().expect("Config value is not a valid string")))
 }
--- src/var.rs
+++ src/var.rs
@@ -17,6 +17,20 @@ pub fn get_memory(key: impl AsRef<str>) -> Result<Option<Memory>, Error> {
     Ok(Some(Memory(memory)))
 }
 
+/// Gets a variable in the plug-in. This variable lives as long as the
+/// plug-in is loaded.
+///
+/// # Arguments
+///
+/// * `key` - A unique string key to identify the variable
+///
+/// # Examples
+///
+/// ```
+/// // let's assume we have a variable at `"my_var"`
+/// // which is a u32. We can default to 0 first time we fetch it:
+/// let my_var = var::get("my_var")?.unwrap_or(0u32);
+/// ```
 pub fn get<T: FromBytesOwned>(key: impl AsRef<str>) -> Result<Option<T>, Error> {
     match get_memory(key)?.map(|x| x.to_vec()) {
         Some(v) => Ok(Some(T::from_bytes(&v)?)),
@@ -24,6 +38,20 @@ pub fn get<T: FromBytesOwned>(key: impl AsRef<str>) -> Result<Option<T>, Error> 
     }
 }
 
+/// Set a variable in the plug-in. This variable lives as long as the
+/// plug-in is loaded. The value must have a [ToMemory] implementation.
+///
+/// # Arguments
+///
+/// * `key` - A unique string key to identify the variable
+/// * `val` - The value to set. Must have a [ToMemory] implementation
+///
+/// # Examples
+///
+/// ```
+/// var::set("my_u32_var", 42u32)?;
+/// var::set("my_str_var", "Hello, World!")?;
+/// ```
 pub fn set(key: impl AsRef<str>, val: impl ToMemory) -> Result<(), Error> {
     let val = val.to_memory()?;
     let key = Memory::from_bytes(key.as_ref().as_bytes())?;
@@ -31,6 +59,18 @@ pub fn set(key: impl AsRef<str>, val: impl ToMemory) -> Result<(), Error> {
     Ok(())
 }
 
+/// Removes a variable from the plug-in. This variable normally lives as long as the
+/// plug-in is loaded.
+///
+/// # Arguments
+///
+/// * `key` - A unique string key to identify the variable
+///
+/// # Examples
+///
+/// ```
+/// var::remove("my_var")?;
+/// ```
 pub fn remove(key: impl AsRef<str>) -> Result<(), Error> {
     let key = Memory::from_bytes(key.as_ref().as_bytes())?;
     unsafe { extism_var_set(key.offset(), 0) };

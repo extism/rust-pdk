65bfe9a517d6f048eb7cea3eebd19f0978717b64
zach <zach@dylibso.com>
2023-10-18 19:16:28 -0700

    chore: bump version

--- examples/count_vowels.rs
+++ examples/count_vowels.rs
@@ -6,15 +6,15 @@ use serde::Serialize;
 const VOWELS: &[char] = &['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'];
 
 #[derive(Serialize)]
-struct TestOutput<'a> {
+struct TestOutput {
     pub count: i32,
-    pub config: String,
-    pub a: String,
-    pub b: &'a str,
+    // pub config: String,
+    // pub a: String,
+    // pub b: &'a str,
 }
 
 #[plugin_fn]
-pub unsafe fn count_vowels<'a>(input: String) -> FnResult<Json<TestOutput<'a>>> {
+pub unsafe fn count_vowels<'a>(input: String) -> FnResult<Json<TestOutput>> {
     let mut count = 0;
     for ch in input.chars() {
         if VOWELS.contains(&ch) {
@@ -22,18 +22,18 @@ pub unsafe fn count_vowels<'a>(input: String) -> FnResult<Json<TestOutput<'a>>> 
         }
     }
 
-    set_var!("a", "this is var a")?;
+    // set_var!("a", "this is var a")?;
 
-    let a = var::get("a")?.expect("variable 'a' set");
-    let a = String::from_utf8(a).expect("string from varible value");
-    let config = config::get("thing")?.expect("'thing' key set in config");
-    let b = "new_value";
+    // let a = var::get("a")?.expect("variable 'a' set");
+    // let a = String::from_utf8(a).expect("string from varible value");
+    // let config = config::get("thing")?.expect("'thing' key set in config");
+    // let b = "new_value";
 
     let output = TestOutput {
         count,
-        config,
-        a,
-        b,
+        // config,
+        // a,
+        // b,
     };
     Ok(Json(output))
 }
